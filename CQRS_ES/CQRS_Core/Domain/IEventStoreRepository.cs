5511f154f1884a34404db5b20678509c5f29970f
guitar_sinner <sinfalling@gmail.com>
2024-11-04 15:19:47 +0400

    EventStoreRepository

---
+++ CQRS_ES/CQRS_Core/Domain/IEventStoreRepository.cs
@@ -0,0 +1,8 @@
+﻿using CQRS_Core.Events;
+
+namespace CQRS_Core.Domain;
+
+public interface IEventStoreRepository {
+	Task SaveAsync(EventModel @event);
+	Task<List<EventModel>> FindByAggregateIdAsync(Guid aggregateId);
+}
---
+++ SM_Post/Post_Cmd/Post_Cmd.Infrastructure/Repositories/EventStoreRepository.cs
@@ -0,0 +1,26 @@
+﻿using CQRS_Core.Domain;
+using CQRS_Core.Events;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using Post_Cmd.Infrastructure.Config;
+
+namespace Post_Cmd.Infrastructure.Repositories;
+
+public class EventStoreRepository: IEventStoreRepository {
+	private readonly IMongoCollection<EventModel> _eventStoreCollection;
+
+	public EventStoreRepository(IOptions<MongoDbConfig> config) {
+		var mongoClient = new MongoClient(config.Value.ConnectionString);
+		var mongoDatabase = mongoClient.GetDatabase(config.Value.Database);
+		this._eventStoreCollection = mongoDatabase.GetCollection<EventModel>(config.Value.Collection);
+	}
+
+	public async Task SaveAsync(EventModel @event) {
+		await this._eventStoreCollection.InsertOneAsync(@event).ConfigureAwait(false);
+	}
+
+	public async Task<List<EventModel>> FindByAggregateIdAsync(Guid aggregateId) {
+		return await this._eventStoreCollection.Find(x => x.AggregationIdentifier == aggregateId).ToListAsync()
+			.ConfigureAwait(false);
+	}
+}

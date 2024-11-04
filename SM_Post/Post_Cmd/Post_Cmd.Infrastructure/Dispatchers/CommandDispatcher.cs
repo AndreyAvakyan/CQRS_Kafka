bda919c871b6c23e7cf39d175d360717cc3b7331
guitar_sinner <sinfalling@gmail.com>
2024-11-04 13:31:40 +0400

    Adding EventModel

--- SM_Post/Post_Cmd/Post_Cmd.Infrastructure/Dispatchers/CommandDispatcher.cs
+++ SM_Post/Post_Cmd/Post_Cmd.Infrastructure/Dispatchers/CommandDispatcher.cs
@@ -7,15 +7,15 @@ public class CommandDispatcher: ICommandDispatcher {
 	private readonly Dictionary<Type, Func<BaseCommand, Task>> _handlers = new();
 
 	public void RegisterHandler<T>(Func<T, Task> handler) where T : BaseCommand {
-		if (_handlers.ContainsKey(typeof(T))){
+		if (this._handlers.ContainsKey(typeof(T))){
 			throw new IndexOutOfRangeException("Handler is already registered!");
 		}
 
-		_handlers.Add(typeof(T), x => handler((T)x));
+		this._handlers.Add(typeof(T), x => handler((T)x));
 	}
 
 	public async Task SendAsync(BaseCommand command) {
-		if (_handlers.TryGetValue(command.GetType(), out var handler)){
+		if (this._handlers.TryGetValue(command.GetType(), out var handler)){
 			await handler(command);
 		} else{
 			throw new ArgumentNullException(nameof(handler), "No command handler was registered!");
